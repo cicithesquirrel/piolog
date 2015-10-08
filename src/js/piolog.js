1531a80bde6e02ba9fdab69cc367fc1043413822
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2015-10-08 08:00:15 +0200

    Fix: Ignorer les lignes incorrectes & Ajouter une indication du numéro de la ligne parsée

--- src/js/piolog.js
+++ src/js/piolog.js
@@ -2,24 +2,31 @@
 
 var matchers = require('./matchers');
 var model = require('./model');
+var log4js = require('log4js');
+var logger = log4js.getLogger("piolog.parser");
 
-function onLine(game, line) {
+function onLine(game, line, currentLineNumber) {
 
     line = line.trim();
 
     if (line.length === 0) {
         return;
     }
-    var hourAndMessage = line.match(/(\d\d:\d\d:\d\d) (.+)$/),
-        hour = hourAndMessage[1],
-        message = hourAndMessage[2];
+    var hourAndMessage = line.match(/(\d\d:\d\d:\d\d) (.+)$/);
 
-    if (!game.startDate) {
-        game.startDate = hour;
+    if (hourAndMessage) {
+        var hour = hourAndMessage[1];
+        var message = hourAndMessage[2];
+
+        if (!game.startDate) {
+            game.startDate = hour;
+        }
+        game.endDate = hour;
+
+        matchers.match(message, game);
+    } else {
+        logger.warn('Incorrect line #' + currentLineNumber + ' has been ignored');
     }
-    game.endDate = hour;
-
-    matchers.match(message, game);
 }
 
 
@@ -27,12 +34,16 @@ exports.parse = function (nextLineFunction) {
 
     var game = model.newGame();
 
+    var currentLineNumber = 1;
+
     var line = nextLineFunction();
     while (line) {
 
-        onLine(game, line);
+        onLine(game, line, currentLineNumber);
 
         line = nextLineFunction();
+
+        currentLineNumber = currentLineNumber + 1;
     }
 
     game.updateScoreOfLastTurn();
