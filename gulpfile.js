9fe92afa02468caefcb1830a1c7b1f4943ab9c73
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2015-11-10 22:28:22 +0100

    Fix: Error when running tests because of gulp task concurrency

--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,13 @@ var paths = {
 };
 
 gulp.task('clean', function (endCallback) {
-    // You can use multiple globbing patterns as you would with `gulp.src` 
-    del(['build', '.coverdata', 'coverage', 'reports', 'debug']);
-    endCallback();
+    del(['.coverdata', 'reports', 'debug', '.coverrun', 'build'], endCallback);
 });
+
+gulp.task('clean-npm', function () {
+    del(['doc']);
+});
+
 
 gulp.task('set-run-env', function (endCallback) {
     env({
@@ -30,10 +33,10 @@ gulp.task('set-run-env', function (endCallback) {
     endCallback();
 });
 
-gulp.task('scripts', ['clean'], function () {
+gulp.task('build', function () {
     // Minify and copy all JavaScript (except vendor scripts) 
     // with sourcemaps all the way down 
-    return gulp.src(paths.scripts)
+    gulp.src(paths.scripts)
         .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(concat(paths.build))
@@ -42,7 +45,7 @@ gulp.task('scripts', ['clean'], function () {
 });
 
 gulp.task('test', ['set-run-env'], function () {
-    return gulp.src(paths.tests, {
+    gulp.src(paths.tests, {
             read: false
         }).pipe(cover.instrument({
             pattern: paths.scripts,
@@ -53,4 +56,4 @@ gulp.task('test', ['set-run-env'], function () {
         .pipe(gulp.dest('reports'));
 });
 
-gulp.task('default', ['clean', 'scripts', 'test']);
+gulp.task('default', ['clean', 'build', 'test']);
